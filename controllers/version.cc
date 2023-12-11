833468eb5b9f0e134e26f1ef65dff4721c204e2d
Ogame3334 <kazuhiroisobe0522@gmail.com>
2023-12-12 06:09:48 +0900

    version name取得ポイント

--- controllers/version.cc
+++ controllers/version.cc
@@ -1,6 +1,43 @@
 #include "version.h"
 
 // Add definition of your processing function here
+void version::get_name( const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& callback, int version_id) const {
+    std::string sql = "SELECT name FROM version WHERE id = $1;";
+    try{
+        auto futureObject = 
+            drogon::app()
+                .getDbClient("default")
+                ->execSqlAsyncFuture(sql, version_id);
+
+        if(std::future_status::timeout == futureObject.wait_for(std::chrono::seconds(3))){
+            Json::Value output;
+            output["state"] = "timeout";
+            callback(HttpResponse::newHttpJsonResponse(output));
+            return;
+        }
+        else{
+            auto result = futureObject.get();
+
+            if(0 >= result.size()){
+                Json::Value output;
+                output["state"] = "error";
+                callback(HttpResponse::newHttpJsonResponse(output));
+            }
+
+            std::string name = result[0]["name"].as<std::string>();
+            Json::Value output;
+            output["state"] = "ok";
+            output["name"] = name;
+            callback(HttpResponse::newHttpJsonResponse(output));
+        }
+    }
+    catch(const drogon::orm::DrogonDbException &e){
+        std::cerr << e.base().what() << '\n';
+        Json::Value output;
+        output["state"] = "error";
+        callback(HttpResponse::newHttpJsonResponse(output));
+    }
+}
 void version::latest( const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& callback) const {
     std::string sql = "SELECT id, name FROM version ORDER BY id DESC LIMIT 1;";
     try{
--- controllers/version.h
+++ controllers/version.h
@@ -12,11 +12,13 @@ class version : public drogon::HttpController<version>
     // METHOD_ADD(version::get, "/{2}/{1}", Get); // path is /version/{arg2}/{arg1}
     // METHOD_ADD(version::your_method_name, "/{1}/{2}/list", Get); // path is /version/{arg1}/{arg2}/list
     // ADD_METHOD_TO(version::your_method_name, "/absolute/path/{1}/{2}/list", Get); // path is /absolute/path/{arg1}/{arg2}/list
+    METHOD_ADD(version::get_name, "/{1}", Get);
     METHOD_ADD(version::latest, "/latest", Get);
 
     METHOD_LIST_END
     // your declaration of processing function maybe like this:
     // void get(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, int p1, std::string p2);
     // void your_method_name(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback, double p1, int p2) const;
+    void get_name( const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& callback, int version_id ) const;
     void latest( const HttpRequestPtr& req, std::function<void(const HttpResponsePtr&)>&& callback ) const;
 };
